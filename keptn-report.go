638333da59c88157008e06fb653aa9a683083276
Christian Heckelmann <christian.heckelmann@ert.com>
2020-04-08 13:48:10 +0200

    Renamed mYValues to chartValues

--- keptn-report.go
+++ keptn-report.go
@@ -114,7 +114,7 @@ func createReport(sourceFile string, reportFile string) {
 	// Array with the Timestamps for the X-Axis of the chart
 	var TimeValues []time.Time
 	// Map for the data of the evaluation metrics and values for the Y-Axis of the chart
-	mYValues := make(map[string][]float64)
+	chartValues := make(map[string][]float64)
 
 	// keptn logo in PNG Format as Base64 encoded String
 	logo := "iVBORw0KGgoAAAANSUhEUgAAAGMAAAAfCAYAAADz23MvAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAZESURBVGhD7VlNVhtHEK4eLGBnH4Eb4JwAe5PAyvIN8CoPsQBOEDgBsAC/rHBOYLKSko3xCQwnwD5B7B0Co0591V2ammF+NEgv8YP53psnqWda01NfV31f9zhqiq0Pz+j6ZpnmOxd0+PJrbG0xA0xOxq/9JZqjLf62Ts49I++ZCHdAnYXDlpTZoJ4MkJC43yhhEorg/Rs6XnsXf7WYAuVk9P56zoHeKiUB8HRIx6vbUrpuri7Ju1N6u/omnm3REEn8TLHZf0G9wQdy/lM1Ef6jEAFcX/H1XLqcD+SCHPxPi0ZIyUAAQQI5DixVB9L7C+osduX7xuCEyxiyiMlZC+TdXJ3I/2z0d+V3i4mQknFzdZAhwftvNKI/+fMP/vEltoZ2StZFtDcH2yF7+PyYHCbAua4QNr94IG0tJkJKhqfl+A0Bx8xf4vrfldl+tLYk+iDn3Dod/3Iey9C+kOOTrpDT66+L2KPtlmIba08TIEM3/l5hUuHcHhUCGQgASg1gA7kx6I6DAn0Y0Wsm6DSIO72XdiUnBH1f2hz3/33ts/SnEZe+Bvg+/IeS0RmP59FlVSDjemg14lwCKbOcA46gaO0HESDOj05EsEd+b9yGoKONaIcz6UzIcT5cB3vcohaBjDrBdpQGE+KMLIKevF0LJKmbgr4crR5kCEN5A7kPCZiovf57nqSfZukaAxner8hnwLIEMyzkduToLAYL2xuwyKs4LwTnlLqpC+4T3dQQGRUIU/v7sIBdiK484wwRyLB/itmMmQ5CMMtxqDg72spoiroptHUWwwwRwjjTMoQ9PjG+D1xIM14T5IG9p1v6KegHxFk1wb8UTdB+IIKSF0HEQZg7ybSBMAj70epk+2CbAx+/0Z0+Yhzc0/iLNeznj/FbCujTk2SFx8Cl1Z/zM1zUlkm4N8Ddclmd+8pa903GXoZe/4yfM1aTGI862LHrfUa3X+zYnIgz7GgeIs6sCcgQbHUETQj7UJYc24ZVuyAOEG4KJsC21aGMjMyEYOT3xGScXB6L9M/7A87cPclmC9T8slKjG6HHq3uxJUWGjAJkxi2VorwyeOKYuB2QnxQOvkyc8fB5cda21MJm3ZRiVGMSqjAREZgwJfdwbjvsCuRQVfNxL0e7ItRTwZffA5Ax87PxMzAZOYZrxdm4Kd0krHJTY1Q8eBXqiAAkI8bnL3hsr2V24lNKJgOCK+ueMnDm6oGqkOlXua3DJsf0laMU6bXZsSGrdxMZvAIn8+IctjrCjCtyU9fD/VLCLBw1t7d3iIiZaCEZaDIC48faB8AnFqApwviKgGzWA1XB9qsqM9Al2xdHKcy1GBsWzGP45+ymkuCGAF05l211qJvK702hLU+YBYjqLNxj09DbGc9lssAm+1F6L2RrXqxtcJzZ8qkD+ulExRjw/LPG/IIZm1tJomvAQ6a1Xrc60C4uCTPUbHWgLaR8IAzOyRJmgfOabU2hi027I1yFhF6JAcgfCrt4nQguDZZv2ncC5GIS1hlI/XytR9204hxmaJE43yUsAyaqzlrWAbpWttJtHOAGEEf13yGQoWARGYuz2toicdbykXdTgbAUIrYVfr0O6K9AtgbCs/BGizAeCHfV0Qj3NB33RJYM1HUQUSTOuiUSvPySXJd/02ehVngaoD8CDAj5nLVCvIWzZL+Kn9MD93FmmyjBekBh7jmNZc8hSwZqGN5hFIkz2mB14Vysm1L7a2Gd1bQA4SAWwH1AvIU4p/jyC5ME47lDGKNu59ieR39MRJ1giEHWCKTZiBJq71d07wmRJUMRBhbrvxFnJUfdlLZlYKzwrICsVGcDQhBwC8+LMwXGgwUgXiGnxyU9cZfximLgPFbkuF7eqZjnci7rBL/7YJ0BTM6bYeinfe/5yqCYDAguyoPWfBFsFwJg3/RpmwJEqRWeJfB/IFgmAgOBsgsxKWdGXzCjEaT0CMGpCxKIxvUWQYeyL7okPvZ+/P96LyBfKSbEpJt32+yuOP24VuqCatxmMKJ3UzsnG2TdklFgUth1Bd6xW+IR7DnHOiZbEJhATzloyCjU+DMm9DRzvbW9IbgIphL2maPDLtOUpzzEUHjOoPGWB549ex9UD7XFRfExz9vQXTwwWDIaO63Zo7hMtfhf0JLxA6El44cB0b/wbe6oMrAKDAAAAABJRU5ErkJggg=="
@@ -137,7 +137,7 @@ func createReport(sourceFile string, reportFile string) {
 			for _, historyResult := range history.Data.Evaluationdetails.IndicatorResults {
 
 				// Append results to the mYValues map like mYValues['response_time_95'][1.0123]
-				mYValues[historyResult.Value.Metric] = append(mYValues[historyResult.Value.Metric], historyResult.Value.Value)
+				chartValues[historyResult.Value.Metric] = append(chartValues[historyResult.Value.Metric], historyResult.Value.Value)
 			}
 		}
 	}
@@ -159,11 +159,11 @@ func createReport(sourceFile string, reportFile string) {
 	// Loop Over the mYValues
 	var series []chart.Series
 	// k is the key like response_time_95
-	for k := range mYValues {
+	for k := range chartValues {
 		series = append(series, chart.TimeSeries{
 			Name:    k,
 			XValues: TimeValues,
-			YValues: mYValues[k],
+			YValues: chartValues[k],
 		})
 	}
 
